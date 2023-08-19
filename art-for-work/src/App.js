a9745e330a931d446cec84e31d9acba7e9cb00ec
Nikhil <ntalmale07@gmail.com>
2023-08-19 23:43:57 +0530

    frontend1

---
+++ art-for-work/src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="navbar navbar-expand-sm bg-light mb3">
+        <div className="container-fluid">
+          <ul  className="navbar-nav">                                                       
+            <li className="nav-item">Home</li>
+          </ul>
+        </div>
+      </nav>
+       <nav className="navbar navbar-expand-sm bg-light mb3">
+        <div className="container-fluid">
+          <ul  className="navbar-nav">                                                       
+            <li className="nav-item">About </li>
+          </ul>
+        </div>
+      </nav>
+       <nav className="navbar navbar-expand-sm bg-light mb3">
+        <div className="container-fluid">
+          <ul  className="navbar-nav">                                                       
+            <li className="nav-item">Gallary</li>
+          </ul>
+        </div>
+      </nav>
+       <nav className="navbar navbar-expand-sm bg-light mb3">
+        <div className="container-fluid">
+          <ul  className="navbar-nav">                                                       
+            <li className="nav-item">Shopping</li>
+          </ul>
+        </div>
+      </nav>
+    
+    </div>
+  );
+}
+
+export default App;
