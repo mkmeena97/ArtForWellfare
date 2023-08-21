9694953d24523085837ad7319c9bed32392f35cf
Nikhil <ntalmale07@gmail.com>
2023-08-21 23:16:03 +0530

    rectcompo

--- art-for-work/src/App.js
+++ art-for-work/src/App.js
@@ -1,59 +1,65 @@
-
 import './App.css';
-import { Link, Routes, Route } from 'react-router-dom';
-import LoginComp from './component/loginComp';
+import 'bootstrap'
+import { Link, Route, Routes } from 'react-router-dom';
+import LoginForm from './component/loginComp';
+import AdminHome from './component/AdminHome';
+import NgoHome from './component/NgoHome';
+import CustomerHome from './component/CustomerHome';
+import ArtistHome from './component/ArtistHome';
+import { useSelector } from 'react-redux';
+import LogoutComp from './component/LogoutComp';
+import ForgotPassword from './component/ForgotPassword';
+import CustomerReg from './component/CustomerReg';
+
 
 
 
 function App() {
+  //referering initial state of logged
+  const mystate = useSelector((state)=>state.logged);
+
+
   return (
-    <div className="App">
-      <nav className="navbar navbar-expand-sm bg-light mb3">
-        <div className="container-fluid">
-          <ul  className="navbar-nav">                                                       
-            <li className="nav-item">
-            <Link to="/" className="nav-link px-3">Home</Link></li>
-          
-                                                          
-            <li className="nav-item">
-            <Link to="/" className="nav-link px-3">About</Link></li>
-
-            <li className="nav-item">
-            <Link to="/" className="nav-link px-3">Gallary</Link></li>
-            <li className="nav-item">
-            <Link to="/LoginComp" className="nav-link px-3">Login</Link></li>
-          </ul>
+    <div>
+        <div style={{display:mystate.loggedIn?"none":"block"}}>
+            <nav className='navbar navbar-expand-sm bg-light mb-3'>
+              <div className='container-fluid'>
+                <div className='collapse navbar-collapse'>
+                  <ul className='navbar-nav mr-auto'>
+                    <li className='nav-item'>
+                      <Link to="login" className='nav-link px-3'>Login</Link>
+                    </li>
+                    <li className='nav-item'>
+                      <Link to="home" className='nav-link px-3'>Home</Link>
+                    </li>
+                    <li className='nav-item'>
+                      <Link to="reg_customer" className='nav-link px-3'>Customer Registration</Link>
+                    </li>
+                    <li className='nav-item'>
+                      <Link to="reg_ngo" className='nav-link px-3'>NGO Registration</Link>
+                    </li>
+                    <li className='nav-item'>
+                      <Link to="reg_artist" className='nav-link px-3'>Artist Registration</Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </nav>
         </div>
-      </nav>
 
-      <footer>
-        <h1>WELCOME TO ART FOR WELLFARE</h1>
-      </footer>
-
-      <div>
-        <ul className="md-4">
-          <li><Link to="/LoginComp" className="nav-link px-3">Login</Link></li>
-          
-          <li><Link to="/" className="nav-link px-3">Register</Link></li>
-          <li><Link to="/" className="nav-link px-3">Feedback</Link></li>
-        </ul>
-        
-        
-
-      
-      </div>
-
-      <Routes>
-          <Route path="/LoginComp" element={<LoginComp/>}/>
-        </Routes>
-
-      
-
-
-
-       
-      
-    
+         
+            <Routes>
+                <Route path='/login' element={<LoginForm></LoginForm>}></Route>
+                <Route path='/admin_home' element={<AdminHome></AdminHome>}></Route>
+                <Route path='/ngo_home' element={<NgoHome></NgoHome>}></Route>
+                <Route path='/customer_home' element={<CustomerHome></CustomerHome>}></Route>
+                <Route path='/artist_home' element={<ArtistHome></ArtistHome>}></Route>
+                <Route path='/logout' element={<LogoutComp></LogoutComp>}></Route>
+                <Route path='/forgotpassword' element={<ForgotPassword></ForgotPassword>}></Route>
+                <Route path='/reg_customer' element={<CustomerReg></CustomerReg>}></Route>
+            </Routes>
+                      
+            <h1 className="bg-primary text-center p-4">Welcome to Art for Welfare Platform</h1>
     </div>
   );
 }
--- art-for-work/src/index.js
+++ art-for-work/src/index.js
@@ -5,14 +5,20 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css' ;
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../src/component/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Provider store={store}>  
   <BrowserRouter>
    <React.StrictMode>
     <App />
+
   </React.StrictMode>
   </BrowserRouter>
+  </Provider>
+
  
 );
 
